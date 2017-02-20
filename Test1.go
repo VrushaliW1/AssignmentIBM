6342bb520814df3b7619e180cadae2d20c9396f3
VrushaliW <vrushali.wagh@mphatek.com>
2017-02-20 16:20:44 +0530

    1.24

--- Test1.go
+++ Test1.go
@@ -1,7 +1,6 @@
 package main
 
-import (
-    "encoding/json"
+import (    
     "errors"
     "fmt"    
     "github.com/hyperledger/fabric/core/chaincode/shim"
@@ -41,4 +40,16 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function stri
     fmt.Println("invoke did not find func: " + function)
 
     return nil, errors.New("Received unknown function invocation")
+}
+
+func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+    fmt.Println("query is running " + function)
+
+    // Handle different functions
+    /*if function == "read" {                            //read a variable
+        return t.read(stub, args)
+    }*/
+    fmt.Println("query did not find func: " + function)
+
+    return nil, errors.New("Received unknown function query")
 }
