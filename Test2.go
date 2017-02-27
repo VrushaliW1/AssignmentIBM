25270aea1cc90889096337e88ce4f312550b6cfe
VrushaliW1 <vrushali.wagh@mphatek.com>
2017-02-27 18:47:52 +0530

    1.150

--- Test2.go
+++ Test2.go
@@ -275,11 +275,11 @@ func (t *SimpleChaincode) createOrUpdateAsset(stub shim.ChaincodeStubInterface, 
     // Check if asset record existed in stub
     assetBytes, err:= stub.GetState(assetID)
     var length int
-    length = len(listAsset)
-    fmt.Println(length)
+    //length = len(listAsset)
+    //fmt.Println(length)
 	//stateStub = stateIn
     //listAsset[len(listAsset)-1:][0]
-    fmt.Println(listAsset[len(listAsset)-1:][0])
+    fmt.Println(listAsset[len(listAsset)-1:])
 	fmt.Println("assetbyte= ", assetBytes)
     if err != nil || len(assetBytes)==0{
         // This implies that this is a 'create' scenario
