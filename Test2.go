706192d6d727b8d29c28dfd74d2fbb81197e8bf6
VrushaliW1 <vrushali.wagh@mphatek.com>
2017-02-28 16:58:47 +0530

    1.176

--- Test2.go
+++ Test2.go
@@ -321,8 +321,9 @@ func (t *SimpleChaincode) createOrUpdateAsset(stub shim.ChaincodeStubInterface, 
 	fmt.Printf("% x", sha1.Sum(bin_buf.Bytes()))    
     buf, err = json.Marshal(bin_buf)
     //_, err = w.Write(buf)
-    fmt.Println("buf==",buf)
+    fmt.Println("buf=",buf)
     err = stub.PutState(assetID, buf)
+    fmt.Println(err)
     if err != nil {
         err = errors.New("PUT ledger state failed: "+ fmt.Sprint(err))  
  	
